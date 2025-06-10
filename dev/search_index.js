4303995c788e731f4dad63f3bf8a316ddc1e3b97
Documenter.jl <documenter@juliadocs.github.io>
2025-06-10 14:25:15 +0000

    build based on a1c2d6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Meteograms","page":"Home","title":"Meteograms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Meteograms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
